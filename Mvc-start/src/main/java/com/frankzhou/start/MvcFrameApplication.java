2628f13509d6bfefa4326203e517ebeddd1f20b4
frankzhou <2280610818@qq.com>
2023-02-18 14:28:41 +0800

    Mvc项目脚手架初始化

--- Mvc-start/src/main/java/com/frankzhou/start/MvcFrameApplication.java
+++ Mvc-start/src/main/java/com/frankzhou/start/MvcFrameApplication.java
@@ -8,6 +8,9 @@ public class MvcFrameApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MvcFrameApplication.class, args);
+        System.out.println("=================================");
+        System.out.println("===========Start-Success=========");
+        System.out.println("=================================");
     }
 
 }
